27fa7a4e6f73d3c83ddbad23817569ac9cb08f95
lizheng <lizheng@hz.com>
2024-06-09 22:38:46 +0800

    faet |> new

---
+++ core/zwsp/kernel/kmod_misc/app/_build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/zw/01_project/03_rk3568/90_work/osmd/kmod_misc/app/codestd.c" "/home/zw/01_project/03_rk3568/90_work/osmd/kmod_misc/app/_build/CMakeFiles/main.dir/codestd.c.o"
+  "/home/zw/01_project/03_rk3568/90_work/osmd/kmod_misc/app/main.c" "/home/zw/01_project/03_rk3568/90_work/osmd/kmod_misc/app/_build/CMakeFiles/main.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
